c04652b9ea20a948194067bfafe94c3e4ba4fb34
MuslemRahimi <moslem_rahimi@hotmail.de>
2025-03-05 11:20:05 +0100

    ui fix

--- src/routes/etf/[tickerID]/+page.svelte
+++ src/routes/etf/[tickerID]/+page.svelte
@@ -716,7 +716,7 @@
                         <li>
                           <button
                             on:click={() => changeData(interval)}
-                            class="px-1 py-1 text-sm sm:text-[1rem] xs:px-[3px] bp:px-1.5 sm:px-2 xxxl:px-3"
+                            class="cursor-pointer px-1 py-1 text-sm sm:text-[1rem] xs:px-[3px] bp:px-1.5 sm:px-2 xxxl:px-3"
                           >
                             <span
                               class="block {displayData === interval
