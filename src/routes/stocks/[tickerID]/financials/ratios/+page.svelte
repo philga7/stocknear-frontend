7956ecfd5ac1703d58b189359ee914680f90d8de
MuslemRahimi <moslem_rahimi@hotmail.de>
2025-04-01 22:31:20 +0200

    typo

--- src/routes/stocks/[tickerID]/financials/ratios/+page.svelte
+++ src/routes/stocks/[tickerID]/financials/ratios/+page.svelte
@@ -389,9 +389,9 @@
                     class="w-11 h-6 bg-gray-400 rounded-full peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-[#1563F9]"
                   ></div>
                   {#if $coolMode}
-                    <span class="ml-2 text-sm"> Chart Mode </span>
-                  {:else}
                     <span class="ml-2 text-sm"> Table Mode </span>
+                  {:else}
+                    <span class="ml-2 text-sm"> Chart Mode </span>
                   {/if}
                 </label>
 
