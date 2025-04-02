463de117e507ba5e24d73c849f43fbda4523cac0
MuslemRahimi <moslem_rahimi@hotmail.de>
2025-04-02 11:36:37 +0200

    update dividends page

--- src/routes/stocks/[tickerID]/dividends/+layout.svelte
+++ src/routes/stocks/[tickerID]/dividends/+layout.svelte
@@ -26,6 +26,48 @@
         </main>
 
         <aside class="hidden lg:block relative fixed w-1/4 mt-3">
+          {#if !["Pro", "Plus"]?.includes(data?.user?.tier) || data?.user?.freeTrial}
+            <div
+              class="w-full border border-gray-300 dark:border-gray-600 rounded-md h-fit pb-4 mt-4 cursor-pointer sm:hover:shadow-lg dark:sm:hover:bg-secondary transition ease-out duration-100"
+            >
+              <a
+                href="/pricing"
+                class="w-auto lg:w-full p-1 flex flex-col m-auto px-2 sm:px-0"
+              >
+                <div class="w-full flex justify-between items-center p-3 mt-3">
+                  <h2 class="text-start text-xl font-semibold sm:ml-3">
+                    Pro Subscription
+                  </h2>
+                </div>
+                <span class=" p-3 sm:ml-3 sm:mr-3 -mt-4">
+                  Upgrade now for unlimited access to all data and tools.
+                </span>
+              </a>
+            </div>
+          {/if}
+
+          <div
+            class="w-full p-2 border border-gray-300 dark:border-gray-600 rounded-md h-fit pb-4 mt-4"
+          >
+            <h3 class="p-2 pt-4 text-xl font-semibold">Dividend Definition</h3>
+            <div class=" p-2">
+              Dividends are payments made by a company to its shareholders,
+              typically derived from its profits. They represent a portion of
+              earnings distributed to investors as a reward for holding shares.
+              Dividends do not account for retained earnings or reinvestments
+              and are therefore different from a company's total profits.
+            </div>
+            <!--
+            <div class="px-2">
+              <a
+                href="/blog/article/revenue"
+                class="flex justify-center items-center rounded cursor-pointer w-full py-2 mt-3 text-[1rem] text-center font-semibold text-white dark:text-black m-auto sm:hover:bg-blue-600 dark:sm:hover:bg-gray-300 bg-[#3B82F6] dark:bg-[#fff] transition duration-100"
+              >
+                Full Definition
+              </a>
+            </div>
+            -->
+          </div>
           {#if similarStocks?.length > 0}
             <div
               class="w-full p-2 border border-gray-300 dark:border-gray-600 rounded-md h-fit pb-4 mt-4"
