4bdcc0a0127a5a833baf2f4e1d97eb07a66b9f10
MuslemRahimi <moslem_rahimi@hotmail.de>
2025-04-22 11:49:13 +0200

    add twitter pixel back

--- src/routes/pricing/+page.svelte
+++ src/routes/pricing/+page.svelte
@@ -59,24 +59,11 @@
         value = 180;
       }
       try {
-        // --- REPLACED: send purchase event to GTM dataLayer ---
-        window.dataLayer = window.dataLayer || [];
-        window.dataLayer.push({
-          event: "Purchase",
-          conversionId: data?.user?.id,
-          advancedMatchingParams: [{ name: "email", value: data.user.email }],
-          itemCount: 1,
+        twq("event", "tw-onuuu-ospg6", {
+          value: value,
           currency: "USD",
-          transactionValue: value,
-          productRows: [
-            {
-              id: subId,
-              category: "Subscription",
-              name: subscriptionType,
-            },
-          ],
+          conversion_id: data?.user?.id,
         });
-        // ---------------------------------------------------------
       } catch (e) {
         console.log(e);
       }
@@ -110,6 +97,24 @@
     window.lemonSqueezyAffiliateConfig = { store: "stocknear" };
   </script>
   <script src="https://lmsqueezy.com/affiliate.js" defer></script>
+
+  <script>
+    !(function (e, t, n, s, u, a) {
+      e.twq ||
+        ((s = e.twq =
+          function () {
+            s.exe ? s.exe.apply(s, arguments) : s.queue.push(arguments);
+          }),
+        (s.version = "1.1"),
+        (s.queue = []),
+        (u = t.createElement(n)),
+        (u.async = !0),
+        (u.src = "https://static.ads-twitter.com/uwt.js"),
+        (a = t.getElementsByTagName(n)[0]),
+        a.parentNode.insertBefore(u, a));
+    })(window, document, "script");
+    twq("config", "onuuu");
+  </script>
 </svelte:head>
 
 <section
