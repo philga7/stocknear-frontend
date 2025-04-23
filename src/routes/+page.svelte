789e9f2b80bb279aa26fcd93cdbc054401fb9637
MuslemRahimi <moslem_rahimi@hotmail.de>
2025-04-23 14:34:37 +0200

    ui fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -112,6 +112,7 @@
         <Feedback {data} />
       {/if}
 
+      <!--
       <div
         class="text-center mb-5 relative w-fit flex justify-center m-auto text-white"
       >
@@ -130,6 +131,7 @@
           >
         </div>
       </div>
+      -->
 
       <h1
         class="hidden sm:block text-3xl lg:text-4xl text-muted dark:text-white font-bold text-center mb-10 relative w-fit flex justify-center m-auto"
