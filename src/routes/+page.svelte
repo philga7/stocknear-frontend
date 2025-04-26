7faddfe4fc63f39a4ef5be6ed4bcfab033c4bcaf
MuslemRahimi <moslem_rahimi@hotmail.de>
2025-04-26 10:42:03 +0200

    bugfixing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -403,7 +403,7 @@
                       {item?.text}
 
                       <a
-                        href={`/stocks/${item?.ticker}`}
+                        href={`/${item?.assetType}/${item?.ticker}`}
                         class="inline-block badge duration-0 bg-blue-50 dark:bg-primary font-semibold dark:font-normal rounded-sm ml-1 px-2 m-auto text-blue-700 dark:text-blue-400 dark:sm:hover:text-white sm:hover:text-muted"
                         >{item?.ticker}</a
                       >
