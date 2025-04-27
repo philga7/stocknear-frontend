6003b0c164cf093a63e697a2a049fdac9d8e267b
MuslemRahimi <moslem_rahimi@hotmail.de>
2025-04-27 13:28:36 +0200

    add web worker for gtag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -143,6 +143,34 @@
     }
       */
 
+    // Initialize dataLayer in main thread
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      "gtm.start": new Date().getTime(),
+      event: "gtm.js",
+    });
+
+    // Create inline web worker
+    const workerCode = `
+      self.postMessage({
+        action: 'loadGTM',
+        url: 'https://www.googletagmanager.com/gtm.js?id=GTM-NZBJ9W63'
+      });
+      self.close();
+    `;
+
+    const blob = new Blob([workerCode], { type: "application/javascript" });
+    const worker = new Worker(URL.createObjectURL(blob));
+
+    worker.onmessage = (event) => {
+      if (event.data.action === "loadGTM") {
+        const script = document.createElement("script");
+        script.async = true;
+        script.src = event.data.url;
+        document.head.appendChild(script);
+      }
+    };
+
     // Clear all the cache every 20 min
     const interval = setInterval(
       () => {
@@ -247,23 +275,6 @@
 </script>
 
 <svelte:window bind:innerWidth={$screenWidth} />
-
-<svelte:head>
-  <!-- Google Tag Manager -->
-  <script>
-    (function (w, d, s, l, i) {
-      w[l] = w[l] || [];
-      w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
-      var f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s),
-        dl = l != "dataLayer" ? "&l=" + l : "";
-      j.async = true;
-      j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-      f.parentNode.insertBefore(j, f);
-    })(window, document, "script", "dataLayer", "GTM-NZBJ9W63");
-  </script>
-  <!-- End Google Tag Manager -->
-</svelte:head>
 
 <ModeWatcher defaultMode={data?.themeMode} />
 
