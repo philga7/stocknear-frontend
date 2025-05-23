544737110b7fa63d4575b30b2d4d263afe97e802
MuslemRahimi <moslem_rahimi@hotmail.de>
2025-05-23 19:10:16 +0200

    ui fix

--- src/lib/components/Chat/ChatMessage.svelte
+++ src/lib/components/Chat/ChatMessage.svelte
@@ -3,7 +3,10 @@
   export let isLoading = false;
 </script>
 
-<div class="flex mb-6" class:justify-end={message.role === "user"}>
+<div
+  class="flex m-auto mb-6 max-w-[1000px]"
+  class:justify-end={message.role === "user"}
+>
   <div class="flex flex-col sm:flex-row items-start">
     <img
       class="mr-auto mb-2 sm:mb-0 sm:mr-3 w-10 h-10 rounded-full {message.role ===
