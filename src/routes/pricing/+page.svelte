e54a4480770d6dd6006265b4e7e9337ae5aa32ca
MuslemRahimi <moslem_rahimi@hotmail.de>
2025-06-09 18:10:16 +0200

    ui fix

--- src/routes/pricing/+page.svelte
+++ src/routes/pricing/+page.svelte
@@ -232,21 +232,6 @@
                 stroke-width="2"
                 d="M5 13l4 4L19 7"
               ></path></svg
-            ><span class="">10 Credits</span>
-          </li>
-          <li class="flex items-start">
-            <svg
-              class="w-5 h-5 mr-2 flex-shrink-0 mt-0.5"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-              ><path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M5 13l4 4L19 7"
-              ></path></svg
             ><span class="">Realtime Notification</span>
           </li>
 
