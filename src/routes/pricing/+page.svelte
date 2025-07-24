93012e4824cb2a2b5a3962b4c8139d82e71f096e
MuslemRahimi <moslem_rahimi@hotmail.de>
2025-07-24 20:42:23 +0200

    ui fix

--- src/routes/pricing/+page.svelte
+++ src/routes/pricing/+page.svelte
@@ -89,7 +89,7 @@
           d="M96 224c35.3 0 64-28.7 64-64s-28.7-64-64-64-64 28.7-64 64 28.7 64 64 64zm448 0c35.3 0 64-28.7 64-64s-28.7-64-64-64-64 28.7-64 64 28.7 64 64 64zm32 32h-64c-17.6 0-33.5 7.1-45.1 18.6 40.3 22.1 68.9 62 75.1 109.4h66c17.7 0 32-14.3 32-32v-32c0-35.3-28.7-64-64-64zm-256 0c61.9 0 112-50.1 112-112S381.9 32 320 32 208 82.1 208 144s50.1 112 112 112zm76.8 32h-8.3c-20.8 10-43.9 16-68.5 16s-47.6-6-68.5-16h-8.3C179.6 288 128 339.6 128 403.2V432c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48v-28.8c0-63.6-51.6-115.2-115.2-115.2zm-223.7-13.4C161.5 263.1 145.6 256 128 256H64c-35.3 0-64 28.7-64 64v32c0 17.7 14.3 32 32 32h65.9c6.3-47.4 34.9-87.3 75.2-109.4z"
         ></path></svg
       ><span class="font-medium text-sm sm:text-lg"
-        >Trusted by <span class="font-semibold">5,000</span>+ Investors</span
+        >Trusted by <span class="font-semibold">6,000</span>+ Investors</span
       >
     </div>
     <span
@@ -814,7 +814,7 @@
                 stroke-width="2"
                 d="M5 13l4 4L19 7"
               ></path></svg
-            ><span class="">Unlimited Watchlist (up to 100 stocks)</span>
+            ><span class="">Unlimited Watchlist up to 100 stocks</span>
           </li>
           <li class="flex items-start">
             <svg
@@ -1302,7 +1302,7 @@
           <label
             for={!data?.user ? "userLogin" : ""}
             on:click={() => purchasePlan("pro")}
-            class="text-white cursor-pointer w-full py-3 px-4 bg-black dark:bg-white rounded-[3px] font-semibold sm:hover:bg-default dark:sm:hover:bg-gray-100 text-white dark:text-black transition duration-100 flex items-center justify-center"
+            class="cursor-pointer w-full py-3 px-4 bg-white rounded-[3px] font-semibold sm:hover:bg-gray-100 dark:sm:hover:bg-gray-100 text-black transition duration-100 flex items-center justify-center"
             >Unlock Pro Access Now<svg
               class="w-5 h-5 ml-2"
               fill="none"
@@ -1410,7 +1410,7 @@
           <label
             for={data?.user ? "" : "userLogin"}
             on:click={() => purchasePlan("lifeTime")}
-            class="text-white cursor-pointer w-full lg:w-auto py-3 lg:mt-2 px-4 bg-black dark:bg-white rounded-[3px] font-semibold sm:hover:bg-default dark:sm:hover:bg-gray-100 text-white dark:text-black transition duration-100 flex items-center justify-center lg:justify-end"
+            class="cursor-pointer w-full lg:w-auto py-3 lg:mt-2 px-4 bg-white rounded-[3px] font-semibold sm:hover:bg-gray-100 dark:sm:hover:bg-gray-100 text-black transition duration-100 flex items-center justify-center lg:justify-end"
           >
             Get Lifetime Now
             <svg
