f060c8bb929debc69d0aec6d9ccb7bc315c97263
MuslemRahimi <mrahimi@stocknear.com>
2025-08-03 21:18:30 +0200

    ui fix

--- src/routes/chat/+page.server.ts
+++ src/routes/chat/+page.server.ts
@@ -8,7 +8,7 @@ export const load = async ({ locals }) => {
   const getAllChats = async () => {
     if (!user) return [];
 
-    const output = await pb.collection("chat").getList(1, 10, {
+    const output = await pb.collection("chat").getList(1, 100, {
       sort: "-updated",
       filter: `user="${user.id}"`,
     });
